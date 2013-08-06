6ff8d3603ba55c600d4a56e63cb1191255cf34b0
Flavio Figueiredo <flaviovdf@gmail.com>
2013-08-06 20:34:10 -0300

    Added new module

---
+++ pyseries/prediction/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8
+from __future__ import print_function, division
+'''
+Prediction
+==========
+
+Implements the following prediction methods:
+    * Radinsky2012
+    * Szabo2010
+    * Pinto2013
+
+Also defines error metrics for prediction:
+    * MSE
+    * MRSE
+    * MAE
+    * Time-to-Surprise-Peak
+    * Time-to-Peak
+    * Growth-to-Peak / Peak
+    * Fraction of remaining views
+'''

030ef5b200d106af4aa0e3f736dda5275d1315ce
Flavio Figueiredo <flaviovdf@gmail.com>
2013-08-06 20:13:40 -0300

    Created module

---
+++ pyseries/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8
+from __future__ import division, print_function
+'''
+PySeries
+========
+
+Time series data mining and prediction in python. This module was created for 
+my PhD research in the need to implement multiple baseline methods for 
+popularity prediction.
+'''
+__version__ = '0.01-git'

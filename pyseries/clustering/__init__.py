8b0f3161a8f3e5ab7757ab80065b636cc8c76036
Flavio Figueiredo <flaviovdf@gmail.com>
2013-08-06 20:39:24 -0300

    Added clustering methods

---
+++ pyseries/clustering/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8
+from __future__ import division, print_function
+'''
+Clustering
+==========
+
+Contains the following clustering methods:
+    * Yang2011
+    * Ahmed2012
+    * Kmeans (from sklearn)
+'''
